67876ee5b37e15212e4da27efb051bee77e67cdb
weichenxi <2641930621@qq.com>
2020-05-13 12:47:50 +0800

    indent the myinit.org

--- init.el
+++ init.el
@@ -20,12 +20,19 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#000000" "#d54e53" "#b9ca4a" "#e7c547" "#7aa6da" "#c397d8" "#70c0b1" "#eaeaea"))
  '(beacon-color "#cc6666")
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-blue)))
+ '(custom-safe-themes
+   (quote
+    ("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
+ '(fci-rule-color "#424242")
  '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
  '(frame-background-mode (quote dark))
  '(package-selected-packages
    (quote
-    (evil-leader geiser ewal-spacemacs-themes spacemacs-theme emacs-theme-gruvbox atom-one-dark haskell-mode slime racket-mode clj-refactor emmet-mode web-mode js2-mode go-mode company-lsp lsp-mode flycheck org-bullets hungry-delete evil-smartparens smartparens yasnippet-snippets yasnippet use-package nyan-mode counsel company cnfonts)))
+    (color-theme-sanityinc-tomorrow evil-leader geiser ewal-spacemacs-themes spacemacs-theme emacs-theme-gruvbox atom-one-dark haskell-mode slime racket-mode clj-refactor emmet-mode web-mode js2-mode go-mode company-lsp lsp-mode flycheck org-bullets hungry-delete evil-smartparens smartparens yasnippet-snippets yasnippet use-package nyan-mode counsel company cnfonts)))
  '(tetris-x-colors
    [[229 192 123]
     [97 175 239]
@@ -34,6 +41,28 @@
     [152 195 121]
     [198 120 221]
     [86 182 194]])
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#d54e53")
+     (40 . "#e78c45")
+     (60 . "#e7c547")
+     (80 . "#b9ca4a")
+     (100 . "#70c0b1")
+     (120 . "#7aa6da")
+     (140 . "#c397d8")
+     (160 . "#d54e53")
+     (180 . "#e78c45")
+     (200 . "#e7c547")
+     (220 . "#b9ca4a")
+     (240 . "#70c0b1")
+     (260 . "#7aa6da")
+     (280 . "#c397d8")
+     (300 . "#d54e53")
+     (320 . "#e78c45")
+     (340 . "#e7c547")
+     (360 . "#b9ca4a"))))
+ '(vc-annotate-very-old-color nil)
  '(window-divider-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
